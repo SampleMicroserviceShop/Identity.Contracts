3b7ca8e26be5fd2e8ab67eed077edb158c82d127
muhammad <karami8071@yahoo.com>
2025-08-23 10:56:31 +0330

    First Commit

---
+++ Contracts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Contracts;
+public record DebitGil(Guid UserId, decimal Gil, Guid CorrelationId);
+public record GilDebited(Guid CorrelationId);
+public record UserUpdated(Guid UserId, string Email, decimal NewTotalGil);
