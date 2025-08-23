5dd799e2a0184ba0c8299e5071bff5c0e49bdb93
muhammad <karami8071@yahoo.com>
2025-08-23 12:32:58 +0330

    removed unused imports

--- Contracts.cs
+++ Contracts.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Identity.Contracts;
+﻿namespace Identity.Contracts;
 public record DebitGil(Guid UserId, decimal Gil, Guid CorrelationId);
 public record GilDebited(Guid CorrelationId);
 public record UserUpdated(Guid UserId, string Email, decimal NewTotalGil);
